c50415ac91e4ddc11594d91f92dafa421f8dc5db
Huei-Horng Yo <hiroshiyui@gmail.com>
2013-09-02 20:45:11 +0800

    Move Sass's config.rb to root dir.
    Signed-off-by: Huei-Horng Yo <hiroshiyui@gmail.com>

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../css"
-sass_dir = "./"
-images_dir = "../images"
-javascripts_dir = "../javascripts"
+css_dir = "./css"
+sass_dir = "./_sass"
+images_dir = "./images"
+javascripts_dir = "./javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
