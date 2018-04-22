6f6acb9c746be0d0fcb382b4311b35782cd62c93
Huei-Horng Yo <hiroshi@ghostsinthelab.org>
2018-04-22 16:46:43 +0800

    Refactoring publish.sh
    Signed-off-by: Huei-Horng Yo <hiroshi@ghostsinthelab.org>

--- publish.sh
+++ publish.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 cd `dirname $0`
+prev=$(git rev-parse HEAD)
+
 git pull -f --no-edit
-rvm . do jekyll build
-cp -R _site/* ~/archlinux.tw/
+
+
+# build only if we have new commit(s)...
+if [ "$prev" != "$(git rev-parse HEAD)" ]
+then
+  echo "We have new commit(s), building an updated site..."
+  rvm . do jekyll build && cp -R _site/* ~/archlinux.tw/
+else
+  echo "No new commit(s), bye!"
+fi
